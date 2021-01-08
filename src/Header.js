f1f3889fff19415347e65c0c57e1bd65ed0e5b80
alexangria1992 <alex040892@gmail.com>
2021-01-08 13:42:11 +1300

    Data Layer set up

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,7 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
+    const [{basket}] = useStateValue()
     return (
         <nav className="header">
             {/* logo on the left -> img */}
@@ -49,7 +50,7 @@ function Header() {
                 <Link to="/checkout" className="header__link">
                     <div className="header__optionBasket">
                     <ShoppingBasketIcon/>
-                        <span className="header__optionLineTwo header__basketCount">0</span>
+                        <span className="header__optionLineTwo header__basketCount">{basket.length}</span>
                     </div>
                 </Link>
                 
---
+++ src/reducer.js
@@ -0,0 +1,22 @@
+export const initialState = {
+    basket: [],
+    
+}
+
+function reducer(state, action)
+{
+    switch(action.type)
+    {
+        case 'ADD_TO_BASKET':
+            //Logic for adding item to basket
+            break;
+        case 'REMOVE FROM BASKET':
+            // Logic for Removing item from basket 
+            break;
+        default:
+        return state;
+    }
+        
+}
+
+export default reducer;
